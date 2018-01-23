eec0bc591ab5eb48132c735c6d9432a5be8d7b1d
ga65fah@mytum.de <Neuseeland22>
2018-01-23 23:52:07 +0100

    Login und Registrieren funktionieren

--- app/src/main/java/de/tum/mw/ftm/praktikum/wghelper/LoginActivity.java
+++ app/src/main/java/de/tum/mw/ftm/praktikum/wghelper/LoginActivity.java
@@ -30,18 +30,18 @@ import java.net.URLEncoder;
 public class LoginActivity extends AppCompatActivity {
 
     //Keys für den Intent
-    public static final String USERNAME = "de.tum.mw.ftm.praktikum.EXTRA_USERNAME";
-    public static final String PASSWORD = "de.tum.mw.ftm.praktikum.EXTRA_PASSWORD";
-    public static final String PREFGROUP_USER = "de.tum.mw.ftm.praktikum.PREFGROUP_USER";
+    public static final String PREFGROUP_WG = "de.tum.mw.ftm.praktikum.android.wghelper.PREFGROUP_WG";
+    public static final String PREFGROUP_USER = "de.tum.mw.ftm.praktikum.android.wghelper.PREFGROUP_USER";
+    public static final String IS_REGISTERED = "de.tum.mw.ftm.praktikum.android.wghelper.IS_REGISTERED";
+    public static final String USERNAME = "de.tum.mw.ftm.praktikum.android.wghelper.USERNAME";
     public static final String USERID = "de.tum.mw.ftm.praktikum.USERID";
     public static final String EMAIL = "de.tum.mw.ftm.praktikum.EMAIL";
     public static final String IS_LOGGED_IN = "de.tum.mw.ftm.praktikum.IS_LOGGED_IN";
-    private static final String REGISTER_URL = "http://pr-android.ftm.mw.tum.de/android/api/praktikum/registrieren.php";
-    private static final String LOGIN_URL = "http://pr-android.ftm.mw.tum.de/android/api/praktikum/login.php";
+    private static final String LOGIN_URL = "http://pr-android.ftm.mw.tum.de/android/wghelper/login.php";
 
     private Button btnRegister;
     private Button btnLogin;
-    private EditText username, wgschluessel;
+    private EditText username, wgpasswort, wg_id;
     private static long backpressed=0;
 
     @Override
@@ -61,35 +61,45 @@ public class LoginActivity extends AppCompatActivity {
 
 
         //Gleich MainActivity starten wenn User noch eingeloggt
-        if (getSharedPreferences(PREFGROUP_USER, MODE_PRIVATE).getBoolean(IS_LOGGED_IN, false)) {
+       /* if (getSharedPreferences(PREFGROUP_USER, MODE_PRIVATE).getBoolean(IS_LOGGED_IN, false)) {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
-        }
+        }*/
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Bundle bundle = getIntent().getExtras();
 
         //Referenz auf Layoutelemente erhalten
         btnLogin = (Button) findViewById(R.id.log_btn);
         username = (EditText) findViewById(R.id.log_user);
-        wgschluessel = (EditText) findViewById(R.id.log_wgschluessel);
+        wgpasswort = (EditText) findViewById(R.id.log_wgschluessel);
+        wg_id = (EditText) findViewById(R.id.log_wg_id);
         btnRegister = (Button) findViewById(R.id.log_btn_new_WG);
 
         //OnClickListener setzen um auf Click Events reagieren zu können
+
+        if (getSharedPreferences(PREFGROUP_WG, MODE_PRIVATE).getBoolean(IS_REGISTERED, false)) {
+            wg_id.setText(bundle.getInt("WG_ID"));
+            wgpasswort.setText(bundle.getString("Passwort"));
+        }
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String strUsername = username.getText().toString();
-                String strPassword = wgschluessel.getText().toString();
-                if(!strPassword.isEmpty() && !strUsername.isEmpty()){
-                    new LoginTask().execute(LOGIN_URL, strUsername, strPassword);
+                String strwgpasswort = wgpasswort.getText().toString();
+                String strwgid = wg_id.getText().toString();
+                if(!strwgpasswort.isEmpty() && !strUsername.isEmpty() && !strwgid.isEmpty()){
+                    new LoginTask().execute(LOGIN_URL, strUsername, strwgid, strwgpasswort);
                 }
             }
         });
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
                 startActivity(intent);
             }
         });
@@ -99,8 +109,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     //Speichert Nutzerdaten als SharedPreferences ab
-    public static class Login {
-        public static void login(long id, String username) {
+        public void login(long id, String username) {
             SharedPreferences.Editor editor = getApplicationContext().getSharedPreferences(PREFGROUP_USER, MODE_PRIVATE).edit();
             editor.putLong(USERID, id).apply();
             editor.putString(USERNAME, username).apply();
@@ -110,7 +119,7 @@ public class LoginActivity extends AppCompatActivity {
 
 
         }
-    }
+
     public class LoginTask extends AsyncTask<String, Void, JSONObject>{
 
 
@@ -125,20 +134,21 @@ public class LoginActivity extends AppCompatActivity {
                 httpURLConnection.setRequestMethod("POST");
                 httpURLConnection.setDoInput(true);
                 httpURLConnection.setDoOutput(true);
-                String data = URLEncoder.encode("name", "UTF-8") + "=" + URLEncoder.encode(params[1], "UTF-8");
-                data += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(params[2], "UTF-8");
+                String data = URLEncoder.encode("Username", "UTF-8") + "=" + URLEncoder.encode(params[1], "UTF-8");
+                data += "&" + URLEncoder.encode("WG_ID", "UTF-8") + "=" + URLEncoder.encode(params[2], "UTF-8");
+                data += "&" + URLEncoder.encode("WG_Passwort", "UTF-8") + "=" + URLEncoder.encode(params[3], "UTF-8");
                 OutputStream out = new BufferedOutputStream(httpURLConnection.getOutputStream());
                 out.write(data.getBytes());
                 out.flush();
                 out.close();
-                StringBuilder sb = new StringBuilder();
+                StringBuilder stringBuilder = new StringBuilder();
                 String line;
-                BufferedReader br = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-                while ((line = br.readLine()) != null) {
-                    sb.append(line);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line);
 
                 }
-                return new JSONObject(sb.toString());
+                return new JSONObject(stringBuilder.toString());
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (JSONException e) {
@@ -157,7 +167,7 @@ public class LoginActivity extends AppCompatActivity {
             if(jsonObject != null){
                 try {
                     if (jsonObject.getBoolean("success")) {
-                        Login.login(jsonObject.getLong("userid"), username.getText().toString());
+                        login(jsonObject.getLong("bewohnerid"), jsonObject.getString("username"));
                     } else {
                         Toast.makeText(getApplicationContext(), "Login nicht erfolgreich", Toast.LENGTH_LONG).show();
 
--- app/src/main/java/de/tum/mw/ftm/praktikum/wghelper/RegistrationActivity.java
+++ app/src/main/java/de/tum/mw/ftm/praktikum/wghelper/RegistrationActivity.java
@@ -1,9 +1,15 @@
 package de.tum.mw.ftm.praktikum.wghelper;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
 import org.json.JSONException;
@@ -25,14 +31,54 @@ import java.net.URLEncoder;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+
+    private static final String REGISTER_URL = "http://pr-android.ftm.mw.tum.de/android/wghelper/registrieren.php";
+    private static final  String WG_ID= "de.tum.mw.ftm.praktikum.wghelper.EXTRA_WGID";
+
+
+    private EditText Passwort, WG_Name;
+    private NumberPicker Anzahl;
+    private Button btn_new_wg;
+    private WG wg;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        //Referenz auf Layoutelemente erhalten
+        btn_new_wg = (Button) findViewById(R.id.log_btn_create_wg);
+        Passwort = (EditText) findViewById(R.id.log_passwort);
+        Anzahl = (NumberPicker) findViewById(R.id.log_teilnehmer);
+        WG_Name = (EditText) findViewById(R.id.log_wgname);
+        Anzahl.setMinValue(1);
+        Anzahl.setMaxValue(20);
+
+
+
+
+
+        btn_new_wg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int strAn = Anzahl.getValue();
+                String strAnzahl = Integer.toString(strAn);
+                String strWG_Name = WG_Name.getText().toString();
+                String strPassword = Passwort.getText().toString();
+
+                if (!strWG_Name.isEmpty() && !strAnzahl.isEmpty() && !strPassword.isEmpty()) {
+                    new NewWG().execute(REGISTER_URL, strWG_Name, strAnzahl, strPassword);
+                }
+
+
+            }
+        });
     }
 
-    public class NewWGTask extends AsyncTask<String, Void, JSONObject> {
+    public class NewWG extends AsyncTask<String, Void, JSONObject> {
 
         @Override
         protected JSONObject doInBackground(String... params) {
@@ -47,10 +93,12 @@ public class RegistrationActivity extends AppCompatActivity {
                 httpURLConnection.setDoInput(true);
                 httpURLConnection.setDoOutput(true);
 //Name und Passwort encoden, damit der Server diese aus dem "name" und "password" Feld auslesen knn
-                String data = URLEncoder.encode("name", "UTF-8")
+                String data = URLEncoder.encode("WG_Name", "UTF-8")
                         + "=" + URLEncoder.encode(params[1], "UTF-8");
-                data += "&" + URLEncoder.encode("password", "UTF-8")
+                data += "&" + URLEncoder.encode("Anzahl", "UTF-8")
                         + "=" + URLEncoder.encode(params[2], "UTF-8");
+                data += "&" + URLEncoder.encode("Passwort", "UTF-8")
+                        + "=" + URLEncoder.encode(params[3], "UTF-8");
 //Daten an Server schicken
                 OutputStream out = new BufferedOutputStream(httpURLConnection.getOutputStream());
                 out.write(data.getBytes());
@@ -64,9 +112,12 @@ public class RegistrationActivity extends AppCompatActivity {
                     sb.append(line);
                 }
                 return new JSONObject(sb.toString());
-            } catch (MalformedURLException e) {e.printStackTrace();
-            } catch (IOException e) {e.printStackTrace();
-            } catch (JSONException e) {e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
             } finally {
                 if (httpURLConnection != null) {
                     httpURLConnection.disconnect();
@@ -78,13 +129,23 @@ public class RegistrationActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(JSONObject jsonObject) {
             super.onPostExecute(jsonObject);
-            if(jsonObject != null){
+            if (jsonObject != null) {
                 try {
-                    if(jsonObject.getBoolean("success")){
-                        LoginActivity.Login(jsonObject.getLong("userid"), username.getText().toString());
-                    }
-                    else {
-                        Toast.makeText(getApplicationContext(), "User existiert bereits",
+                    if (jsonObject.getBoolean("success")) {
+                        Intent i = new Intent(RegistrationActivity.this, LoginActivity.class);
+
+                        try {
+
+                            i.putExtra("WG", jsonObject.getString("WG"));
+                            SharedPreferences.Editor editor = getApplicationContext().getSharedPreferences(LoginActivity.PREFGROUP_WG,MODE_PRIVATE).edit();
+                            editor.putBoolean(LoginActivity.IS_REGISTERED, true);
+                            startActivity(i);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "WG existiert bereits oder es ist etwas schief gelaufen!",
                                 Toast.LENGTH_LONG).show();
                     }
                 } catch (JSONException e) {
@@ -93,4 +154,51 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         }
     }
-}
+   public class WG{
+
+        private String WG_Name, Passwort;
+        private int WG_ID;
+        private Float Anzahl;
+
+       public WG(String WG_Name, String passwort, int WG_ID, Float anzahl) {
+           this.WG_Name = WG_Name;
+           Passwort = passwort;
+           this.WG_ID = WG_ID;
+           Anzahl = anzahl;
+       }
+
+       public String getWG_Name() {
+           return WG_Name;
+       }
+
+       public void setWG_Name(String WG_Name) {
+           this.WG_Name = WG_Name;
+       }
+
+       public String getPasswort() {
+           return Passwort;
+       }
+
+       public void setPasswort(String passwort) {
+           Passwort = passwort;
+       }
+
+       public int getWG_ID() {
+           return WG_ID;
+       }
+
+       public void setWG_ID(int WG_ID) {
+           this.WG_ID = WG_ID;
+       }
+
+       public Float getAnzahl() {
+           return Anzahl;
+       }
+
+       public void setAnzahl(Float anzahl) {
+           Anzahl = anzahl;
+       }
+   }
+
+    }
+
